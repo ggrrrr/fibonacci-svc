1a41f25fcdc6e4ef13e4df713a82b8aef62f387b
vesko <ggrrrr@gmail.com>
2024-02-06 00:21:17 +0200

    add api handlers

--- common/api/api_test.go
+++ common/api/api_test.go
@@ -16,16 +16,19 @@ func TestSend(t *testing.T) {
 		Message: "msg",
 	})
 	assert.Equal(t, `{"payload":"data","code":200,"message":"msg"}`, testWriter.Output)
+	assert.Equal(t, 200, testWriter.StatusCode)
 }
 
 func TestSendError(t *testing.T) {
 	testWriter := http.TestResponseWriter{}
 	SendError(&testWriter, errors.New("error msg"))
 	assert.Equal(t, `{"code":500,"message":"error msg"}`, testWriter.Output)
+	assert.Equal(t, 500, testWriter.StatusCode)
 }
 
 func TestSendPayload(t *testing.T) {
 	testWriter := http.TestResponseWriter{}
 	SendPayload(&testWriter, "data")
 	assert.Equal(t, `{"payload":"data","code":200}`, testWriter.Output)
+	assert.Equal(t, 200, testWriter.StatusCode)
 }
